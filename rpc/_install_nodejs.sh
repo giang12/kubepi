0f1180710a75da119a73eb2f221ac344424281d0
Zen Nguyen <nggiang12@gmail.com>
2019-03-03 23:47:34 -0800

    install nodejs

---
+++ rpc/_install_nodejs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+user=$1
+address=$2
+
+ssh $user@$address << EOF
+if ! node -v; then
+  echo "Setting up node"
+  
+  wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+  sudo dpkg -i node_latest_armhf.deb
+
+else
+ echo "Nodejs already installed"
+ npm version
+fi
+EOF
